31d2edb54c967affa6a7e95906d43d15f9137bef
Waqar1616 <143096150+Waqar1616@users.noreply.github.com>
2023-08-25 22:03:55 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94740804536', 'Dinuwa Official²⁰²³', true],
-  ['94740804536', 'Dinuwa Official²⁰²³', true], 
+  ['94740804536', 'waqar', true],
+  ['94740804536', 'waqar', true], 
   [''] 
 ] //Number of owners
 
